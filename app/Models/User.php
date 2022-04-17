e08481ca3ca620089c902f21c1f05b98a16e09ec
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-04-17 15:22:04 +0000

    make some changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
+use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
