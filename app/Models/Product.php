cfe29016108623d2a66018ef14bd2cd42186f693
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-05-01 14:50:30 +0000

    format currency

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Akaunting\Money\Currency;
+use Akaunting\Money\Money;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
 use Jenssegers\Mongodb\Eloquent\SoftDeletes;
+use Prophecy\Doubler\Generator\TypeHintReference;
 
 class Product extends Model
 {
@@ -30,5 +33,11 @@ class Product extends Model
     {
         return $this->belongsTo(User::class,'created_by')
             ->withDefault(['full_name' => __('messages.user_deleted')]);
+    }
+
+    public function getPriceAttribute()
+    {
+        $format = new Money($this->attributes['price'],new Currency(config('app.currency')));
+        return $format->getAmount() . ' ' . $format->getCurrency();
     }
 }
