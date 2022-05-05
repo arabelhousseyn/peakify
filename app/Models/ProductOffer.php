143f3e6c5d2261310b69bf97ecab41c8ac8f50cf
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-05-05 13:36:58 +0000

    create productOfferModel

---
+++ app/Models/ProductOffer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
+
+class ProductOffer extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'quantity',
+        'discount',
+        'is_static'
+    ];
+}
---
+++ database/migrations/2022_05_05_133348_create_product_offers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('quantity');
+            $table->string('discount');
+            $table->boolean('is_static');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_offers');
+    }
+};
