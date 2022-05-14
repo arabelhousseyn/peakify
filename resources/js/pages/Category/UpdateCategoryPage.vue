6db7a40029640cb2c51532394310b88b3d163dcd
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-05-14 09:27:22 +0000

    make some changes

--- resources/js/pages/Category/UpdateCategoryPage.vue
+++ resources/js/pages/Category/UpdateCategoryPage.vue
@@ -6,7 +6,7 @@
             <v-card width="650" class="mt-5" elevation="0">
                 <v-card-title>Modifier une catégories</v-card-title>
                 <v-card-text>
-                    <form class="flex justify-content-center mb-3" @submit.prevent="update">
+                    <form class="flex justify-content-center mb-3" @submit.prevent="handle">
                         <v-row>
                             <v-col cols="12" lg="6" sm="6">
                                 <v-text-field
@@ -37,10 +37,12 @@
                 </v-card-text>
             </v-card>
         </v-container>
+        <confirmation-update-user-dialog :dialog="dialog" @close="close" @update="update" />
     </div>
 </template>
 <script>
 import BreadCrumbsComponent from "../../components/BreadCrumbsComponent";
+import ConfirmationUpdateUserDialog from "../../components/dialog/ConfirmationUpdateUserDialog";
 export default {
     props : ['data'],
     data : ()=>({
@@ -67,12 +69,22 @@ export default {
         hasError : false,
         errors : [],
         disable : false,
-        infos : {}
+        infos : {},
+        dialog : false,
     }),
-    components: {BreadCrumbsComponent},
+    components: {ConfirmationUpdateUserDialog, BreadCrumbsComponent},
     methods : {
+        handle()
+        {
+            this.dialog = true
+        },
+        close()
+        {
+            this.dialog = false
+        },
         update()
         {
+            this.dialog = false
             this.loading = true
             this.disabled = true
 
