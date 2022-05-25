118055e2dd4a39a9dd76beece4b22555469cfb2c
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-05-25 08:06:41 +0000

    create model OrderProduct

---
+++ app/Models/OrderProduct.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class OrderProduct extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'has_variants'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class,'product_id')->withDefault([]);
+    }
+}
